c69d176f9e3f3c87eb59c3f41875686bf73ff6c6
Yuma Nemoto <nemoto1257@gmail.com>
2018-12-09 21:36:52 +0900

    add wrapped logger

--- main.go
+++ main.go
@@ -19,6 +19,24 @@ func CurrentTimeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("the current time is %v", curTime)))
 }
 
+//Logger is a middleware handler that does request logging
+type Logger struct {
+	handler http.Handler
+}
+
+//ServeHTTP handles the request by passing it to the real
+//handler and logging the request details
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	l.handler.ServeHTTP(w, r)
+	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+}
+
+//NewLogger constructs a new Logger middleware handler
+func NewLogger(handlerToWrap http.Handler) *Logger {
+	return &Logger{handlerToWrap}
+}
+
 func main() {
 	err := godotenv.Load("./local.env")
 	if err != nil {
@@ -31,8 +49,12 @@ func main() {
 	mux.HandleFunc("/v1/hello", HelloHandler)
 	mux.HandleFunc("/v1/time", CurrentTimeHandler)
 
+	//wrap entire mux with logger middleware
+	wrappedMux := NewLogger(mux)
+
 	log.Printf("server is listening at %s", addr)
-	log.Fatal(http.ListenAndServe(addr, mux))
+	//use wrappedMux instead of mux as root handler
+	log.Fatal(http.ListenAndServe(addr, wrappedMux))
 }
 
 /*
